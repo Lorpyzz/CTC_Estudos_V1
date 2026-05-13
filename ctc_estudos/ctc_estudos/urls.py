d20decff6ba8d1c591556c5e0f9a11a99482d1eb
gabrielnovaispaiva <gabriel.novais.paiva@gmail.com>
2026-05-13 11:26:41 -0300

    fluxo de telas funcionando normalmente(algumas telas precisam de conserto)

--- ctc_estudos/ctc_estudos/urls.py
+++ ctc_estudos/ctc_estudos/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from views import disciplinas, home, flashcards
+from views import disciplinas, home, flashcards, duvidas, chat, login
 
 urlpatterns = [
     path('', home.home, name='home'),
     path('disciplinas/', disciplinas.lista_disciplinas, name='lista_disciplinas'),
     path('flashcards/', flashcards.flashcards, name='flashcards'),
+    path('duvidas/', duvidas.duvidas, name='duvidas'),
+    path('chat/', chat.chat, name='chat'),
+    path('login/', login.login, name='login'),
+
 ]
