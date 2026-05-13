60cfae873cd9fb02763bf95c311416ae80839a63
lorpyzz <jvlopes.costa14@gmail.com>
2026-05-13 14:45:43 -0300

    pagina de flashcard
    - criar o modal
    - colocar banco de dados mockados no modal
    - fazer com que os decks criados fiquem na pagina principal

--- ctc_estudos/views/flashcards.py
+++ ctc_estudos/views/flashcards.py
@@ -1,4 +1,18 @@
+import json
 from django.shortcuts import render
 
 def flashcards(request):
-    return render(request, 'flashcards.html')
+    dados_json = """
+    {
+      "disciplinas": [
+        {"id": 1, "nome": "Programação",                    "codigo": "INF1000"},
+        {"id": 2, "nome": "Estruturas de Dados",            "codigo": "INF1010"},
+        {"id": 3, "nome": "Estruturas de Dados Avançadas",  "codigo": "INF1100"},
+        {"id": 4, "nome": "Cálculo I",                      "codigo": "MAT1000"},
+        {"id": 5, "nome": "Cálculo II",                     "codigo": "MAT1110"}
+      ]
+    }
+    """
+    dados = json.loads(dados_json)
+    disciplinas = dados["disciplinas"]
+    return render(request, 'flashcards.html', {"disciplinas": disciplinas})
