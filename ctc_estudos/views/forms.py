415d540079dcd9b6e13a0c72016c1d508a4e50c9
gabrielnovaispaiva <gabriel.novais.paiva@gmail.com>
2026-05-20 18:23:35 -0300

    adição de disciplinas mockada

--- ctc_estudos/views/forms.py
+++ ctc_estudos/views/forms.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from views.disciplinas import disciplinas_lista
 
 def forms(request):
-    return render(request, 'forms.html')
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        codigo = request.POST.get("codigo")
+        descricao = request.POST.get("descricao")
+        departamento = request.POST.get("departamento")
+
+        nova_disciplina = {
+            "id": len(disciplinas_lista) + 1,
+            "nome": nome,
+            "codigo": codigo,
+            "descricao": descricao,
+            "departamento": departamento,
+        }
+
+        disciplinas_lista.append(nova_disciplina)
+        return redirect("lista_disciplinas")
+
+    return render(request, "forms.html")
